e24d342a9f283eb1a00662b348ec6937e94c037e
Boris Fernando HC <bhuancac@est.emi.edu.bo>
2021-09-23 11:38:09 -0400

    BOTON CUADRADO

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-function AreaCuadrado(lado){
-    const area = lado * lado;
-    return area;
-}
-const perimetroCuadrado =  lado => lado * 4;
+var perimetroDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * 4;
+var areaDelCuadrado = (ladoDelCuadrado) => ladoDelCuadrado * ladoDelCuadrado;
+
+
+
+
+// const ladoDelCuadrado = 6;
+// const PI = 3.1416;
+// let variableQueCambia
+// const area = lado * lado;
+// const perimetro = lado * 4;
+
+// console.log(`Area: ${area}`);
+// console.group({ area: area, perimetro: perimetro })
